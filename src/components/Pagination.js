de4c196d0b27a13cc172eeb7caaf0d4f55682543
Aasim Taif <taifullahansari@gmail.com>
2023-03-07 05:19:21 +0530

    working on search property

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Dashboard from './Dashboard';
 import "../Pagination.css";
 import { useSelector } from 'react-redux';
@@ -10,27 +10,40 @@ import { useSelector } from 'react-redux';
 
 
 function Pagination() {
-    const data = useSelector(state => state.data.info)
-console.log(data)
+
+    const [data, setData] = useState();
+
+    const info = useSelector(state => state.data.info)
+    
+    const [input, setInput] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage, setPostsPerPage] = useState(10);
+    useEffect(() => {
+        setData(info)
+        let filteredData = info?.filter(data => data?.name.includes(input))
+        setData(filteredData)
+    }, [input]);
+
+
+    console.log(data?.length)
+
     let pages = [];
 
     for (let i = 1; i <= Math.ceil(data?.length / postsPerPage); i++) {
         pages.push(i);
     }
 
-    if (!data) {
+    if (!info) {
         return <>wait </>
     }
     const lastPostIndex = postsPerPage * currentPage
     const firstPostIndex = lastPostIndex - postsPerPage
-    console.log(firstPostIndex, lastPostIndex)
+    // console.log(firstPostIndex, lastPostIndex)
     const currentPost = data?.slice(firstPostIndex, lastPostIndex)
-
+    console.log(input)
     return (
         <div className='table'>
-            <input placeholder={"Search by  name , id , email..."} />
+            <input placeholder={"Search by  name , id , email..."} onChange={(e) => setInput(e.target.value.toLowerCase())} />
             <Dashboard data={currentPost} />
             <div className='pagination'>
                 {pages.map((page, index) => {
